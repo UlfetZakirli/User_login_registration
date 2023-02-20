038c46f50e58c656d01c7ab2d2eb4083fc5a0fee
UlfetZakirli <ulfetzakirli@gmail.com>
2023-02-20 13:59:44 +0400

    api-feat: hashed and decoded password

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/User.js')
 const Joi = require('joi')
+const bcrypt = require('bcryptjs')
 
 const router = express.Router()
 
@@ -16,19 +17,22 @@ const registerValidate = (user) => {
 const loginValidate = (user) => {
     const schema = Joi.object({
         email: Joi.string().required(),
-        password: Joi.string().require()
+        password: Joi.string().required()
     })
     return schema.validate(user)
 }
 
 router.post('/register', (req, res) => {
     const { name, email, password } = req.body
-    const user = new User(req.body)
 
     const { error } = registerValidate(req.body)
     if (error) {
         return res.status(404).json(error.details[0].message)
     }
+    const salt = bcrypt.genSaltSync(10)
+    const hash = bcrypt.hashSync(password, salt)
+    const user = new User({ ...req.body, password: hash })
+
     user.save()
         .then((user) => {
             res.json(user)
@@ -45,6 +49,21 @@ router.post('/login', (req, res) => {
     if (error) {
         return res.status(404).json(error.details[0].message)
     }
+    User.findOne({ email })
+        .then((user) => {
+            if (!user) {
+                return res.status(400).json('Invalid email or password!')
+            }
+            const isValid = bcrypt.compareSync(password, user.password)
+            if (!isValid) {
+                return res.status(400).json('Invalid email or password!')
+            }
+            res.json(user)
+        })
+        .catch(() => {
+            res.status(400).json('Invalid email or password!')
+        })
+
 
 })
 
